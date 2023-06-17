645d265cbd1fcb8748e552a97f5586448214228d
Lautaro Exequiel Fernández <81919816+Lautarof25@users.noreply.github.com>
2023-06-17 12:41:31 -0300

    refactorizando y agregando funciones

--- source/js/script.js
+++ source/js/script.js
@@ -16,64 +16,52 @@ const button = document.querySelector("#addItem")
 const list = document.querySelector("#list")
 
 // Agrego evento
-inputItem.addEventListener("keyup",addItemKeyboard)
-button.addEventListener("click",addItemButton)
-// Template list
+inputItem.addEventListener("keyup", addItemKeyboard)
+button.addEventListener("click", addItemButton)
 
 
-function addItemKeyboard(e){
-  let item = e.target.value.trim().toLowerCase()
-  if (e.key == 'Enter' && item != "" && !arrayItems.includes(item)){
-      arrayLength = arrayItems.length
-      arrayItems.push(item)
-      list.innerHTML +=
-      `
-      <div class="flex justify-between items-center	">
-        
-        <ul class="flex items-center justify-between gap-2"><input id="item${arrayLength}" type="checkbox" value="" class="w-4 h-4">  ${item} </ul>
-        <button>X</button>
-      </div>
-      `
-      arrayItems.push(inputItem.value)
-      inputItem.value = ""
-    }
-    if(arrayItems.includes(item)){
-      warningDiv.ìnnerHTML = `<p>"El item ya está en la lista"</p>`
-    }
+function addItemKeyboard(e) {
+  let item = removeAccents(inputItem.value.trim().toLowerCase())
+  if (e.key === 'Enter' && arrayItems.includes(item)) {
+    warningMessage()
+  }
+  if (e.key === 'Enter' && item != "" && !arrayItems.includes(item)) {
+    addUl(item)
+  }
+
 }
 
-function addItemButton(){
-  let item = inputItem.value.trim().toLowerCase()
-  if(item != "" && !arrayItems.includes(item)){
-    arrayLength = arrayItems.length
-    arrayItems.push(item)
-    list.innerHTML +=
-      `
-      <div class="flex justify-between items-center	">
-        
-        <ul class="flex justify-between items-center gap-2"><input id="item${arrayLength}" type="checkbox" value="" class="w-4 h-4"> ${item} </ul>
-        <button>X</button>
-      </div>
-      `
+function addItemButton() {
+  let item = removeAccents(inputItem.value.trim().toLowerCase())
+  if (arrayItems.includes(item)) {
+    warningMessage()
+  }
+  if (item !== "" && !arrayItems.includes(item)) {
+    addUl(item)
+  }
+
+}
+
+function warningMessage(){
+  warningDiv.innerHTML = `<p class="bg-red-500 text-center">Ya está en la lista</p>`
+  setTimeout(() => {
+    warningDiv.innerHTML = ""
+    inputItem.value = ""
+    inputItem.focus()
+  }, 3000);
+}
+
+function addUl(item) {
+  arrayLength = arrayItems.length
+  list.innerHTML +=
+    `
+  <div class="flex justify-between items-center	">
+  
+  <ul class="flex justify-between items-center gap-2"><input id="item${arrayLength}" type="checkbox" value="" class="w-4 h-4"> ${item} </ul>
+  <button>X</button>
+  </div>
+  `
+  arrayItems.push(item)
   inputItem.value = ""
   inputItem.focus()
-  }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
